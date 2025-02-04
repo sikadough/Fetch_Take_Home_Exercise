814f7972d32b1949efea2b7ebb5d245cc13737d6
sikadough <63256659+sikadough@users.noreply.github.com>
2025-02-04 01:32:40 -0600

    Add files via upload

---
+++ Data Quality Checks.sql
@@ -0,0 +1,56 @@
+
+---------------SQL Queries for Data Quality Checks
+
+--1. Missing Data in Critical Fields
+-- Check for null values in Receipts
+SELECT COUNT(*) AS missing_receipt_data
+FROM Receipts
+WHERE userId IS NULL OR dateScanned IS NULL OR totalSpent IS NULL;
+
+-- Check for null values in Users
+SELECT COUNT(*) AS missing_user_data
+FROM Users
+WHERE _id IS NULL OR createdDate IS NULL;
+
+-- Check for null values in Brands
+SELECT COUNT(*) AS missing_brand_data
+FROM Brands
+WHERE _id IS NULL OR brandCode IS NULL;
+
+-------2.Inconsistent Data in rewardsReceiptStatus
+-- Check for invalid status values
+SELECT rewardsReceiptStatus, COUNT(*) AS status_count
+FROM Receipts
+GROUP BY rewardsReceiptStatus
+HAVING rewardsReceiptStatus NOT IN ('Accepted', 'Rejected', 'Pending', 'Finished');
+
+--------3. Duplicate Records
+-- Find duplicate receipts
+SELECT receipt_id, COUNT(*) AS duplicate_count
+FROM Receipts
+GROUP BY _id
+HAVING COUNT(*) > 1;
+
+-- Find duplicate users
+SELECT user_id, COUNT(*) AS duplicate_count
+FROM Users
+GROUP BY user_id
+HAVING COUNT(*) > 1;
+
+-- Find duplicate brands
+SELECT brand_id, COUNT(*) AS duplicate_count
+FROM Brands
+GROUP BY _id
+HAVING COUNT(*) > 1;
+
+--4. Referential Integrity Checks
+SELECT COUNT(*) AS orphaned_receipts
+FROM Receipts r
+LEFT JOIN Users u ON r.userId = u._id
+WHERE u.user_id IS NULL;
+
+-- 5.Check for orphaned receipt items (brandCode not in Brands)
+SELECT COUNT(*) AS orphaned_items
+FROM ReceiptItems ri
+LEFT JOIN Brands b ON ri.brandCode = b.brandCode
+WHERE b.brandCode IS NULL;
---
+++ SQL Queries.sql
@@ -0,0 +1,60 @@
+-- 1.  What are the top 5 brands by reciepts scanned for most recent month?
+
+SELECT TOP 5
+   b.NAMES
+   ,COUNT(r.receipt_id) AS Receipt_Count
+FROM Brands b
+JOIN ReceiptItems ri 
+ON b.brandCode = ri.brandCode 
+JOIN Receipts r 
+ON r.receipt_id = re.receipt_id
+WHERE datetrunc(month,purchaseDate) = datetrunc(month, getdate())
+GROUP BY  b.names
+ORDER BY Receipt_Count ;
+
+
+-- 3. When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+
+SELECT 
+    rewardsReceiptStatus,
+    AVG(totalSpent)
+WHERE rewardsReceiptStatus IN ('Accepted', 'Rejected');
+
+-- 4. When considering total number of items purchased from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+
+SELECT 
+    rewardsReceiptStatus,
+    SUM(purchasedItemCount)
+WHERE rewardsReceiptStatus IN ('Accepted', 'Rejected');
+
+-- 5. Which brand has the most spend among users who were created within the past 6 months?
+
+SELECT  TOP 1 
+    b.name as Brand_name
+    ,SUM(r.totalSpent) as Tot_Spend
+FROM Receipts r 
+JOIN Users u
+ON u.user_id = r.user_id
+JOIN ReceiptItems ri 
+ON r.receipt_id = ri.eceipt_id
+JOIN Brands b 
+ON b.brandCode = ri.brandCode 
+WHERE createdDate >= DATEDADD(month, -6, GETDATE())
+GROUP BY b.name
+ORDER BY 2 DESC;
+
+-- 6. Which brand has the most transactions among users who were created within the past 6 months
+
+SELECT  TOP 1 
+    b.name as Brand_name
+    ,COUNT(r.totalSpent) as Tot_Transaction
+FROM Receipts r 
+JOIN Users u
+ON u.user_id = r.user_id
+JOIN ReceiptItems ri 
+ON r.receipt_id = ri.eceipt_id
+JOIN Brands b 
+ON b.brandCode = ri.brandCode 
+WHERE createdDate > DATEDADD(month, -6, GETDATE())
+GROUP BY b.name
+ORDER BY Tot_Transaction DESC;
